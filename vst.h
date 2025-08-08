bc16fce6991a6fcec3db5cdfb896ae29ac9b0ebc
Xaymar <info@xaymar.com>
2025-08-08 03:59:29 +0200

    Fix up C99 support again
    We still require a C++ compiler that treats C header files as actual C code.

--- vst.h
+++ vst.h
@@ -620,7 +620,7 @@ struct vst_event_t {
  */
 union vst_event_midi_t {
 	/** Shared event structure. */
-	vst_event_t event;
+	struct vst_event_t event;
 
 	struct {
 		/** @private */
@@ -679,7 +679,7 @@ union vst_event_midi_t {
  */
 union vst_event_midi_sysex_t {
 	/** Shared event structure. */
-	vst_event_t event;
+	struct vst_event_t event;
 
 	struct {
 		/** @private */
@@ -724,7 +724,7 @@ struct vst_events_t {
 	 *
 	 * The size of this array is defined by @ref vst_events_t.count.
 	 */
-	vst_event_t** events;
+	struct vst_event_t** events;
 };
 
 //------------------------------------------------------------------------------------------------------------------------
